5cd924e6627dea4e20e9e60ee70e64c14e137fed
Mateus Alencar <mateuus.png@gmail.com>
2023-01-26 15:34:45 -0300

    chore: add Material Entity

---
+++ src/main/java/br/com/autoflex/entity/Material.java
@@ -0,0 +1,21 @@
+package br.com.autoflex.entity;
+
+public class Material {
+  private String name;
+  private int stock;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getStock() {
+    return stock;
+  }
+
+  public void setStock(int stock) {
+  }
+}
