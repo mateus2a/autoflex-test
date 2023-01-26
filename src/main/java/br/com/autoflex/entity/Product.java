00d1645ca547ea98fc7a1ffe1a6c97b26671717f
Mateus Alencar <mateuus.png@gmail.com>
2023-01-26 14:32:21 -0300

    update: Product Entity

--- src/main/java/br/com/autoflex/entity/Product.java
+++ src/main/java/br/com/autoflex/entity/Product.java
@@ -1,7 +1,12 @@
 package br.com.autoflex.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
+@Entity
+@Table(name = "products")
 public class Product extends PanacheEntity {
   private String name;
   private Double price;
