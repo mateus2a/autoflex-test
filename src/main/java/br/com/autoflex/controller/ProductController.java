b2ec4e6f98219249c0843c8742dfa34d88ce6d8f
Mateus Alencar <mateuus.png@gmail.com>
2023-01-26 15:10:36 -0300

    chore: finishing Product Controller

--- src/main/java/br/com/autoflex/controller/ProductController.java
+++ src/main/java/br/com/autoflex/controller/ProductController.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import br.com.autoflex.dto.ProductDto;
 import br.com.autoflex.entity.Product;
 import br.com.autoflex.service.ProductService;
 
@@ -24,5 +29,28 @@ public class ProductController {
   public Response listProduct() {
     List<Product> products = service.listProduct();
     return Response.ok(products).build();
+  }
+
+  @POST
+  public Response saveProduct(ProductDto dto) {
+    Product product = service.saveProduct(dto);
+
+    return Response.ok(product).status(Response.Status.CREATED).build();
+  }
+
+  @PUT
+  @Path("{id}")
+  public Response updateProduct(@PathParam("id") Long id, ProductDto dto) {
+    Product product = service.updateProduct(id, dto);
+
+    return Response.ok(product).status(Response.Status.CREATED).build();
+  }
+
+  @DELETE
+  @Path("{id}")
+  public Response removeProduct(@PathParam("id") Long id) {
+    service.removeProduct(id);
+
+    return Response.status(204).build();
   }
 }
--- src/main/java/br/com/autoflex/service/ProductService.java
+++ src/main/java/br/com/autoflex/service/ProductService.java
@@ -3,12 +3,26 @@ package br.com.autoflex.service;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 
+import br.com.autoflex.dto.ProductDto;
 import br.com.autoflex.entity.Product;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 @ApplicationScoped
-public class ProductService {
+public class ProductService implements PanacheRepository<Product> {
   public List<Product> listProduct() {
     return Product.listAll();
+  }
+
+  @Transactional
+  public Product saveProduct(ProductDto dto) {
+    Product product = new Product();
+    product.setName(dto.getName());
+    product.setPrice(dto.getPrice());
+
+    product.persist();
+
+    return product;
   }
 }
